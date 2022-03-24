d3794768a6a60ed03e4ad2c0ba2db97efbdb7024
Senchuu <senchuuuu@gmail.com>
2022-03-24 21:44:13 +0100

    Update project.

--- tests/index.rb
+++ tests/index.rb
@@ -1,1 +1,11 @@
+# frozen_string_literal: true
 
+require_relative "../lib/sruby/index"
+
+db = Sruby::Database.new
+test = db.create("test")
+db.test == test
+test.insert("one", 1)
+test.insert(Hash["two", 2, "three", 3])
+test.all
+db.all("test")
